e58465bb6f1582c92939b2d74514549c0edde6b6
LeandroCepeda <48310079+LeandroCepeda@users.noreply.github.com>
2020-02-14 22:06:52 -0300

    Spring Security
    Se agrega un login para el panel de administración

---
+++ src/main/java/com/integrador/ProyectoIntegradorV1/seguridad/WebSecurityConfig.java
@@ -0,0 +1,37 @@
+package com.integrador.ProyectoIntegradorV1.seguridad;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	
+	protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+            .and()
+            .logout()
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login?logout")
+                .permitAll();
+    }
+	
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	      auth.inMemoryAuthentication()
+	      .withUser("admin").password("{noop}entretrazosdh").roles("USER");
+ }
+
+}
