2b08907480bcb752f210448b70fca14b18876547
LeandroCepeda <48310079+LeandroCepeda@users.noreply.github.com>
2020-02-15 00:31:14 -0300

    Cross Origin Solucionado
    Se permite hacer peticiones desde fetch a la API REST

--- src/main/java/com/integrador/ProyectoIntegradorV1/seguridad/WebSecurityConfig.java
+++ src/main/java/com/integrador/ProyectoIntegradorV1/seguridad/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.integrador.ProyectoIntegradorV1.seguridad;
 
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,8 +14,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	
 	protected void configure(HttpSecurity http) throws Exception {
-        http
+		http.csrf().disable()
             .authorizeRequests()
+            .antMatchers(
+                    "/api/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
